6f2e739e88382dd76fb1dba672616c620fca34da
eyadmohamed20 <mimoeyad14@gmail.com>
2025-07-04 19:17:50 +0300

    product done

---
+++ src/shippableProduct.java
@@ -0,0 +1,13 @@
+public class shippableProduct extends Product implements shippable {
+    private double weight;
+
+    public shippableProduct(String name, double price, int quantity, double weight) {
+        super(name, price, quantity);
+        this.weight = weight;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+}
